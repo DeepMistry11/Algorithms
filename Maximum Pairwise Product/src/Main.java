eab9c6424cbb55e6d51020b0a02262daa58c7a96
Deep Mistry <deepmistry753@gmail.com>
2021-12-25 18:05:26 -0500

    Assignment 1 completed

--- Maximum Pairwise Product/src/Main.java
+++ Maximum Pairwise Product/src/Main.java
@@ -1,6 +1,36 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int length;
+        Scanner ip = new Scanner(System.in);
+        length = ip.nextInt();
+
+        int[] numbers = new int[length];
+        System.out.println("Enter the " + length + " numbers now.");
+
+        for (int i = 0 ; i < numbers.length; i++ ) {
+            numbers[i] = ip.nextInt();
+        }
+
+        Arrays.sort(numbers);
+
+        for (int i = 0 ; i < numbers.length; i++ ) {
+            System.out.println("Sorted array: " + numbers[i]);
+        }
+
+        int maxProduct;
+
+        if(length > 1){
+            maxProduct = numbers[length-1] * numbers[length-2];
+            System.out.println("Max Product is: " + maxProduct);
+        }else{
+            System.out.println("You need atleast 2 numbers to find max product ");
+        }
+
     }
 }
