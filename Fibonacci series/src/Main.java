821078ed6783e0f9bbc9ba5dd92090c8cf932abb
Deep Mistry <deepmistry753@gmail.com>
2022-01-08 22:47:54 -0500

    Insertion Sort algorithm tried

--- Fibonacci series/src/Main.java
+++ Fibonacci series/src/Main.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        Fibo series = new Fibo();
-        series.calculate(50);
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        calculate(n);
     }
+
+
+        public static void calculate(int input){
+            long[] Numbers = new long[input];
+            Numbers[0] = 0;
+            Numbers[1] = 1;
+            for(int i = 2; i < input; i++ ){
+                Numbers[i] = Numbers[i-1] + Numbers[i-2];
+                System.out.println(Numbers[input]);
+            }
+
+        }
+
 }
--- Greatest common divisor/src/Main.java
+++ Greatest common divisor/src/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
     Divisor div = new Divisor();
-    div.GCD(7845,9885);
+    div.GCD(117845,900885);
     }
 }
